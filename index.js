a9f3283bad3c9c4658d72aac4e1c751ced6acdfa
mangojun <99588166+mangojun@users.noreply.github.com>
2022-12-22 06:57:00 +0900

    v 0.3

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
-blocksite = [
-    "youtube.com"
-]
+// blocksite = [
+//     "youtube.com"
+// ]
 
-setInterval(()=>{
-    let loc = window.location.href;
+// setInterval(()=>{
+//     let loc = window.location.href;
 
-    function check(site){
-        if(loc.includes(site)){
-            alert("하지마");
-            history.back();
-        }
+//     function check(site){
+//         if(loc.includes(site)){
+//             alert("하지마");
+//             history.back();
+//         }
+//     }
+
+//     blocksite.forEach(site => check(site));
+// }, 500)
+
+csst = `
+<style>
+#grp {
+    position:fixed;
+    bottom:0;
+    right:0;
+    background-color:#0080FF;
+    margin:50px;
+    font-size:20px;
+    box-shadow: 2px 2px 5px;
+    padding: 10px;
+    border-radius: 22.5px;
+    width: 25px;
+}
+
+#lst {
+    list-style: none;
+    padding: 10px 0
+}
+
+    li {
+        margin-bottom: 10px;
     }
 
-    blocksite.forEach(site => check(site));
-}, 500)
+    .close {
+        display: none;
+        transition: all ease 2s 0s;
+    }
+
+    .open {
+        display: inline;
+    }
+</style>
+`
+
+htmlt = `
+    <div id="grp">
+        <ol id="lst" class="close" onselectstart="return false">
+            <li>🚫</li>
+            <li>🌐</li>
+            <li>🤬</li>
+        </ol>
+        <div id="btn">🔍</div>
+    </div>
+`
+document.querySelector("body").innerHTML += htmlt
+document.querySelector("head").innerHTML += csst
+
+let open = false
+document.getElementById("btn").addEventListener("click", ()=>{
+    if(open){
+        open = false
+        document.getElementById("lst").className = "close"
+    } else {
+        open = true
+        document.getElementById("lst").className = "open"
+    }
+})
